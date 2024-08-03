bf43a6be09404c2d04ee3d362881fbc9343d92c5
Neeru Pareek <neerupareek18@gmail.com>
2024-08-03 22:29:24 +0530

    Practice Ex.

---
+++ src/PracticePrograms_27072024/Lab140.java
@@ -0,0 +1,13 @@
+package PracticePrograms_27072024;
+
+public class Lab140 {
+    public static void main(String[] args) {
+        for(float f=0.0f; f<9;f++){
+            System.out.println(f);
+
+            String s = "44";
+            int i = Integer.parseInt(s);
+            System.out.println(i+1);
+        }
+    }
+}
